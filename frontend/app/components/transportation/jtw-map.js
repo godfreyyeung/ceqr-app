1b8369b0bd618cbb667c8528397a029ef409ba25
godfreyyeung <godfreyyeung@gmail.com>
2019-06-21 12:49:10 -0400

    move intercardinal lines to jtw-map

--- frontend/app/components/transportation/jtw-map.js
+++ frontend/app/components/transportation/jtw-map.js
@@ -1,4 +1,23 @@
 import Component from '@ember/component';
+import { computed, action } from '@ember-decorators/object';
 
 export default class TransportationJtwMapComponent extends Component {
+    /**
+   * The transportation-analysis Model, passed down from the project/show/transportation-analysis controller
+   */
+  analysis = {};
+
+    /**
+   * The highlighted features, already determined during previous "study area" step:
+   * - user-selected study selection features
+   * - required study selection features
+   * which are passed to the highlight layer's FeatureFilterer
+   */
+  @computed('analysis.{jtwStudySelection.[],requiredJtwStudySelection.[]}')
+  get highlightedFeatureIds() {
+    const selectedFeatures = this.get('analysis.jtwStudySelection') || [];
+    const requiredSelectedFeatures = this.get('analysis.requiredJtwStudySelection') || [];
+
+    return [...selectedFeatures, ...requiredSelectedFeatures];
+  }
 }
