fbb0221f7094b731dae2998ed730925f63cef5e5
godfreyyeung <godfreyyeung@gmail.com>
2019-06-24 13:58:40 -0400

    merge stash after renaming col

--- backend/app/models/transportation_analysis.rb
+++ backend/app/models/transportation_analysis.rb
@@ -10,7 +10,7 @@ class TransportationAnalysis < ApplicationRecord
     compute_traffic_zone!
   end
 
-  private
+#  private
 
     # Find, set, and save the traffic zone
     def compute_traffic_zone!
@@ -44,56 +44,121 @@ class TransportationAnalysis < ApplicationRecord
     end
 
     def compute_jtw_aggregates
-      compute_quadrant_polygons
-      # compute_aggregates
+      centroid_pt_text = get_centroid_pt_text(self.jtw_study_area_centroid)
+      intercardinal_pts_text = compute_intercardinal_pts(centroid_pt_text)
+      compute_quadrant_lines(centroid_pt_text, intercardinal_pts_text)
+      # compute_quadrant_polygons
+      # compute_quadrant_geoids
+
+    end
+
+    def compute_intercardinal_pts(centroid_pt_text)
+      nw_pt_text = get_intercardinal_pt_text(centroid_pt_text, "315")
+      ne_pt_text = get_intercardinal_pt_text(centroid_pt_text, "45")
+      sw_pt_text = get_intercardinal_pt_text(centroid_pt_text, "135")
+      se_pt_text = get_intercardinal_pt_text(centroid_pt_text, "225")
+      return { "nw" => nw_pt_text, "ne" => ne_pt_text, "sw" => sw_pt_text, "se" => se_pt_text }
+    end
+
+    def compute_quadrant_lines(centroid_pt_text, intercardinal_pts_text)
+      nw_line_sql = get_sql_of_intercardinal_line_geojson(centroid_pt_text, intercardinal_pts_text["nw"])
+      nw_line_geojson =  ActiveRecord::Base.connection.execute(nw_line_sql).first["st_asgeojson"]
+      ne_line_sql = get_sql_of_intercardinal_line_geojson(centroid_pt_text, intercardinal_pts_text["ne"])
+      ne_line_geojson =  ActiveRecord::Base.connection.execute(ne_line_sql).first["st_asgeojson"]
+      sw_line_sql = get_sql_of_intercardinal_line_geojson(centroid_pt_text, intercardinal_pts_text["sw"])
+      sw_line_geojson =  ActiveRecord::Base.connection.execute(sw_line_sql).first["st_asgeojson"]
+      se_line_sql = get_sql_of_intercardinal_line_geojson(centroid_pt_text, intercardinal_pts_text["se"])
+      se_line_geojson =  ActiveRecord::Base.connection.execute(se_line_sql).first["st_asgeojson"]
+
+      self.nw_line_geojson = nw_line_geojson
+      self.ne_line_geojson = ne_line_geojson
+      self.sw_line_geojson = sw_line_geojson
+      self.se_line_geojson = se_line_geojson
     end
 
     def compute_quadrant_polygons
-      compute_quadrant_lines
-      # compute_quadrant_polygons here
+      n_polygon_sql = get_sql_of_quadrant(self.centroid_pt, self.ne_pt_text, self.nw_pt_text)
+      n_polygon_geojson =  ActiveRecord::Base.connection.execute(n_polygon_sql).first["st_asgeojson"]
+      e_polygon_sql = get_sql_of_quadrant(self.centroid_pt, self.ne_pt_text, self.se_pt_text)
+      e_polygon_geojson =  ActiveRecord::Base.connection.execute(e_polygon_sql).first["st_asgeojson"]
+      s_polygon_sql = get_sql_of_quadrant(self.centroid_pt, self.sw_pt_text, self.se_pt_text)
+      s_polygon_geojson =  ActiveRecord::Base.connection.execute(s_polygon_sql).first["st_asgeojson"]
+      w_polygon_sql = get_sql_of_quadrant(self.centroid_pt, self.sw_pt_text, self.nw_pt_text)
+      w_polygon_geojson =  ActiveRecord::Base.connection.execute(w_polygon_sql).first["st_asgeojson"]
+
+      self.n_polygon_geojson = n_polygon_geojson
+      self.e_polygon_geojson = e_polygon_geojson
+      self.s_polygon_geojson = s_polygon_geojson
+      self.w_polygon_geojson = w_polygon_geojson
     end
 
-    def compute_quadrant_lines
-      nw_line_sql = get_sql_of_line_at_azimuth_from_centroid(self.jtw_study_area_centroid, "315")
-      nw_line =  ActiveRecord::Base.connection.execute(nw_line_sql).first["st_asgeojson"]
-      ne_line_sql = get_sql_of_line_at_azimuth_from_centroid(self.jtw_study_area_centroid, "45")
-      ne_line =  ActiveRecord::Base.connection.execute(ne_line_sql).first["st_asgeojson"]
-      sw_line_sql = get_sql_of_line_at_azimuth_from_centroid(self.jtw_study_area_centroid, "135")
-      sw_line =  ActiveRecord::Base.connection.execute(sw_line_sql).first["st_asgeojson"]
-      se_line_sql = get_sql_of_line_at_azimuth_from_centroid(self.jtw_study_area_centroid, "225")
-      se_line =  ActiveRecord::Base.connection.execute(se_line_sql).first["st_asgeojson"]
-
-      self.nw_line_geojson = nw_line
-      self.ne_line_geojson = ne_line
-      self.sw_line_geojson = sw_line
-      self.se_line_geojson = se_line
-
+    def get_centroid_pt_text(centroid)
+      return "'Point(" << centroid.x.to_s << " " << centroid.y.to_s << ")'"
     end
 
-    def get_sql_of_line_at_azimuth_from_centroid(centroid, azimuth)
+    def get_intercardinal_pt_text(centroid_pt_text, azimuth)
+      intercardinal_pt_sql = "SELECT ST_AsText("
+      intercardinal_pt_sql << "   ST_Project("
+      intercardinal_pt_sql << "     ST_GeographyFromText("
+      intercardinal_pt_sql <<         centroid_pt_text
+      intercardinal_pt_sql << "     ),"
+      intercardinal_pt_sql << "     100000,"
+      intercardinal_pt_sql << "      radians(" << azimuth <<  ")"
+      intercardinal_pt_sql << "   )"
+      intercardinal_pt_sql << " )"
+
+      return "'" << ActiveRecord::Base.connection.execute(intercardinal_pt_sql).first["st_astext"] << "'"
+    end
+
+    def get_sql_of_intercardinal_line_geojson(centroid_pt_text, intercardinal_pt_text)
       line_sql = "SELECT ST_AsGeoJSON("
       line_sql << "  ST_MakeLine(ARRAY["
       line_sql << "      ST_GeomFromText("
-      line_sql << "            'Point(" << centroid.x.to_s << " " << centroid.y.to_s << ")', 3857"
+      line_sql << "            " << centroid_pt_text << ", 3857"
       line_sql << "        ),"
       line_sql << "      St_GeomFromText("
-      line_sql << "        ST_AsText("
-      line_sql << "          ST_Project("
-      line_sql << "            ST_GeographyFromText("
-      line_sql << "                'Point(" << centroid.x.to_s << " " << centroid.y.to_s << ")'"
-      line_sql << "            ),"
-      line_sql << "            100000,"
-      line_sql <<              "radians(" << azimuth <<  ")"
-      line_sql << "          )"
-      line_sql << "        ),"
-      line_sql << "        3857"
+      line_sql <<             intercardinal_pt_text
+      line_sql <<            ", 3857"
       line_sql << "      )"
       line_sql << "    ]"
       line_sql << "  )"
       line_sql << ")"
+
       return line_sql
     end
 
+    def get_sql_of_quadrant(centroid, pt_a, pt_b)
+      quadrant_sql =  "SELECT ST_AsGeoJSON("
+      quadrant_sql << "  ST_Polygon("
+      quadrant_sql << "   ST_MakeLine(ARRAY["
+      quadrant_sql << "       ST_GeomFromText("
+      quadrant_sql <<             centroid 
+      quadrant_sql << "         , 4326),"
+      quadrant_sql << "       St_GeomFromText("
+      quadrant_sql <<             pt_a
+      quadrant_sql << "         , 4326),"
+      quadrant_sql << "       St_GeomFromText("
+      quadrant_sql <<             pt_v
+      quadrant_sql << "         , 4326),"
+      quadrant_sql << "       ST_GeomFromText("
+      quadrant_sql <<             centroid
+      quadrant_sql << "         , 4326)z,"
+      quadrant_sql << "     ]"
+      quadrant_sql << "   ),"
+      quadrant_sql << "   4326"
+      quadrant_sql << "  )"
+
+      return quadrant_sql
+    end
+
+    # def get_quadrant_geoids(quadrant_polygon_geojson)
+    #   line_sql = "SELECT geoid FROM ctpp_censustract_centroids WHERE"
+    #   line_sql <<  "ST_CONTAINS("
+    #   line_sql <<    "ST_GeomFromGeoJSON(" << quadrant_polygon_geojson << "),"
+    #   line_sql <<    "ST_GeographyFromText(CENTROID)"
+    #   line_sql <<  ")"
+    #   return line_sql
+    # end
 
     # def compute_aggregates
     #   compute_geoids_per_quadrant
